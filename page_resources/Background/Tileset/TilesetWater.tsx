42ca19c533eab9776a3d989dc0688acabdd2a8af
venghour <hengvenghour7@gmail.com>
2025-09-04 23:35:20 +1000

    remove unused file

--- page_resources/Background/Tileset/TilesetWater.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="TilesetWater" tilewidth="16" tileheight="16" tilecount="476" columns="28">
- <image source="../../../../../../../Rendering resource/Game assets/Ninja Adventure - Asset Pack/Ninja Adventure - Asset Pack/Backgrounds/Tilesets/TilesetWater.png" width="448" height="272"/>
-</tileset>
--- page_resources/Background/Tileset/Vegetation.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Vegetation" tilewidth="16" tileheight="16" tilecount="504" columns="24">
- <image source="../../../../../../../Rendering resource/Game assets/Ninja Adventure - Asset Pack/Ninja Adventure - Asset Pack/Backgrounds/Tilesets/TilesetNature.png" width="384" height="336"/>
-</tileset>
